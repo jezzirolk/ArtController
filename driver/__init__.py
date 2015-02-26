5aa829be4679bed47a2a824220500e529e779edd
Max Dubrinsky <mimbsky102@gmail.com>
2015-02-25 20:02:25 -0500

    Updated the robot side a bit and implemented a simple pwm setter on the driver side.

--- driver/__init__.py
+++ driver/__init__.py
@@ -1,10 +1,27 @@
 import socket
+import sys
+import time
 
-s = socket.socket()
-host = socket.gethostname()
+driver_ip = socket.gethostname()
 port = 1857
-s.bind((host, port))
+s = socket.socket()
+robot_connection = (driver_ip, port)
 
-s.connect((host, port))
-print(s.recv(1024))
+while 1:
+    try:
+        s.connect(robot_connection)
+        print 'Robot connected'
+        break
+    except:
+        print('Waiting for connection...')
+        time.sleep(2)
+
+while True:
+    # Temporarily does basic PWM input for testing purposes
+    num = input('Enter a PWM number: ')
+    val = input('Enter a signal value: ')
+    msg = 'pwm%d%5d' % (num, val)
+    print("Sending %s", msg)
+    s.sendall(msg)
+
 s.close()
--- robot/__init__.py
+++ robot/__init__.py
@@ -6,23 +6,24 @@ import sys
 # TODO: GPIO, I2C, UART
 pwm = ["P9_14", "P9_16", "P9_21", "P9_22", "P9_42", "P8_13", "P8_19"]
 ain = ["P9_33", "P9_35", "P9_36", "P9_37", "P9_38", "P9_39", "P9_40"]
+packetLoss = 5  # Optimizing 
 
 ldrive = 0
 rdrive = 1
 
 # Setup data socket
 s = socket.socket()
-driver_address = ('localhost', 1857) # Change from localhost eventually
+driver_address = (socket.gethostname(), 1857)  # Change from localhost eventually
 s.bind(driver_address)
 s.listen(1)
 
 while True:
-    print >>sys.stderr, 'Waiting for Driver connection...'
+    print 'Waiting for Driver connection...'
     connection, client_address = s.accept()
     try:
-        print >>sys.stderr, 'Got a connection from', client_address
+        print 'Got a connection from', client_address
         
-        # Start PWM values at '0'
+        # Start PWM values at pseudo-zero
         for st in pwm:
             PWM.start(st, 50)
         
@@ -32,23 +33,25 @@ while True:
             
             if msg == 'drv':
                 # Drive command was sent
-                PWM.set_duty_cycle(pwm[ldrive], connection.recv(4))
-                PWM.set_duty_cycle(pwm[rdrive], connection.recv(4))
+                PWM.set_duty_cycle(pwm[ldrive], float(connection.recv(5)))
+                PWM.set_duty_cycle(pwm[rdrive], float(connection.recv(5)))
                 
             elif msg == 'pwm':
                 # PWM signal command was sent
-                PWM.set_duty_cycle(pwm[connection.recv(1)], connection.recv(4))
+                PWM.set_duty_cycle(pwm[connection.recv(1)], float(connection.recv(5)))
                 
             elif msg == 'cls':
                 # Received the disconnect command
+                print 'Driver disconnected, cleaning up...'
+                connection.sendall('GG')
                 break
             
+    finally:
         # Stop PWM signals and clean up
         for st in pwm:
             PWM.stop(st)
         PWM.cleanup()
         
-        print >>sys.stderr, 'Driver disconnected, cleaning up...'
-    finally:
         # Clean up connection object
         connection.close()
+s.close()
