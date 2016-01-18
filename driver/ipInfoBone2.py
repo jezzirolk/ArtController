9a3e296f58cccf88b41def58101f0c25e379a93f
jezzirolk <schmitzzy@gmail.com>
2016-01-18 14:59:51 -0500

    fixed other class

--- driver/ipInfoBone2.py
+++ driver/ipInfoBone2.py
@@ -1,10 +1,10 @@
 import sys,os
 	
-class ipInfo(object):
+class IpInfo(object):
 	#add the ip info for this machine and symlink it to the ipInfo.py	
 
 
-	def __init__(self, c):
+	def __init__(self):
 		self.ip = '127.0.0.1'
 		self.port = '2001'
 
