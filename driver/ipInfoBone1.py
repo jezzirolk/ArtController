661438a6d5b8c1c6a89ed05c95a0dde8ee779b15
Jezzirolk <schmitzzy@gmail.com>
2016-01-18 22:21:03 -0500

    changed bone1 ip

--- driver/ipInfoBone1.py
+++ driver/ipInfoBone1.py
@@ -3,6 +3,6 @@ import sys,os
 class IpInfo(object):
 	#add the ip info for this machine and symlink it to the ipInfo.py	
 	def __init__(self):
-		self.ip = '127.0.0.1'
+		self.ip = '192.168.1.40'
 		self.port = 2000
 
