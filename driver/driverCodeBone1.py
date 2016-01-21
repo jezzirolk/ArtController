1d00108cec9d9db18acf14532c0a21ea0180136e
Jezzirolk <schmitzzy@gmail.com>
2016-01-20 19:38:04 -0500

    senst

--- driver/driverCodeBone1.py
+++ driver/driverCodeBone1.py
@@ -54,8 +54,9 @@ class DriverCode(object):
 		if Rpot2 != self.radioPot2:
 			self.radioPot2 = Rpot2
 			print 'Radio Pot 2 - %s' % (Rpot2)
-		if (cargearpos > (self.gearPos + .005)) or (cargearpos < (self.gearPos - .005)):
+		if (cargearpos > (self.gearPos + .03)) or (cargearpos < (self.gearPos - .03)):
 			if self.gearDeb == 0:
+				self.gearDeb = 10
 				print 'gearChanged'
 		if self.gearDeb > 0:
 			self.gearDeb = self.gearDeb - .1
